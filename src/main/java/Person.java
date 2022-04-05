9656453dd6ffa3ec455a5414dbd40ea1dcb79268
Denis Alimov <de.alimov@mail.ru>
2022-04-05 17:58:25 +0300

    Initial commit

---
+++ src/main/java/Person.java
@@ -0,0 +1,71 @@
+import java.util.Objects;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    private int age;
+    private String address;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public boolean hasAge() {
+        return age != 0;
+    }
+
+    public boolean hasAddress() {
+        return !address.equals("");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void happyBirthday() {
+        age++;
+    }
+
+    public PersonBuilder newChildBuilder() {
+
+        PersonBuilder child = new PersonBuilder();
+        child.setAge(1);
+        child.setSurname(getSurname());
+        child.setAddress(getAddress());
+        return child;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + surname + ", возраста " + age + " год, проживающий(ая) в городе " + address;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getSurname());
+    }
+}
